4f4dbea856c1ed3eebce792319c7ba8641efebc8
Leenton Chinyanga <Leenton.Chinyanga@futurenet.com>
2024-04-04 16:23:08 +0200

    Created issues

--- MeetingRoom/MeetingRoom.py
+++ MeetingRoom/MeetingRoom.py
@@ -1,21 +1,14 @@
 import sqlite3
 
+from Issue import Issue
+
 class MeetingRoom():
-    def __init__(self, id, name):
+    def __init__(self, id: int, name: str, floor_id: int):
         self.id = id
         self.name = name
-        self.floor = None
-     
-    def get_floors() -> list[Floor]:
-        conn = sqlite3.connect("data.db")
-        c = conn.cursor()
-        c.execute("SELECT floor_name, floor_id FROM floors WHERE room_id = ?", (self.id,))
-        floors = c.fetchall()
-        conn.close()
-        
-        return [Floor(floor[0], floor[1]) for floor in floors]
+        self.floor_id = floor_id
     
-    def get_Issues() -> list[Issue]:
+    def get_Issues(self) -> list[Issue]:
         conn = sqlite3.connect("data.db")
         c = conn.cursor()
         c.execute("SELECT issue_name, issue_id FROM issues WHERE room_id = ?", (self.id,))
