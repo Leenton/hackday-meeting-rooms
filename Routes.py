77f44c1d0b17e96e51dc5b658ecef478b087f728
UserHasBeenName <sonny.pugh.125@accesstomusic.ac.uk>
2024-04-04 14:46:36 +0100

    add get_office function

--- Routes.py
+++ Routes.py
@@ -40,14 +40,34 @@ def get_offices() -> list[Office]:
 
     return [Office(office[0], office[1]) for office in offices]
 
+def get_office(office_id: str) -> Office:
+    # Grabs an office by ID.
+    conn = sqlite3.connect("data.db")
+    c = conn.cursor()
+    
+    try:
+        c.execute("SELECT * FROM offices WHERE office_id = $1", (office_id,))
+        office = c.fetchone()
+        print(office)
+        conn.close()
+        return Office(name = office[0], id=office[1])
+    except sqlite3.OperationalError:
+        # Make an error message that will be displayed in place of the office name
+        conn.close()
+        return Office(name="Cannot find office", id=0)
+
 @app.route("/")
 def home():
     return render_template("home.html", title="Home", offices=get_offices()) 
 
-@app.route("/building/<>")
-def anison():
-    return render_template("building.html")
+@app.route("/offices/<office_id>")
+def office(office_id):
+    # Get the office by id from the list of offices
+    get_office(office_id)
+
+    office = Office()
+    return render_template("building.html", )
 
 intialize_database()
-app.run(debug=True)
+
 
