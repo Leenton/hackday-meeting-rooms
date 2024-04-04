17b410678a355c09f6288bde6647f1ddd00ca354
Leenton Chinyanga <Leenton.Chinyanga@futurenet.com>
2024-04-04 18:58:06 +0200

    Add meeting room logic

--- MeetingRoom/MeetingRoom.py
+++ MeetingRoom/MeetingRoom.py
@@ -7,16 +7,20 @@ class MeetingRoom():
         self.id = id
         self.name = name
         self.floor_id = floor_id
-    
-    def get_Issues(self) -> list[Issue]:
+
+    def get_issues(resolved:bool, meeting_room_id:int) -> list[Issue]:
         conn = sqlite3.connect("data.db")
         c = conn.cursor()
-        c.execute("SELECT issue_name, issue_id FROM issues WHERE room_id = ?", (self.id,))
-        issues = c.fetchall()
-        conn.close()
+        try:
+            c.execute("SELECT issue_note, author_email, created_date, is_resolved, fresh_ticket_id meeting_room_id FROM issues WHERE is_resolved = $1 AND meeting_room_id = $2", (int(resolved) , meeting_room_id))
+            issues = c.fetchall()
+            conn.close()
+            return [Issue(issue[0],issue[1],issue[2],issue[3],issue[4],issue[5]) for issue in issues]
+        except:
+            conn.close()
+            raise Exception("Cannot find meeting room")
         
-        return [Issue(issue[0], issue[1]) for issue in issues]
-
+        
 def get_meeting_room(meeting_room_id: int) -> MeetingRoom:
     conn = sqlite3.connect("data.db")
     c = conn.cursor()
