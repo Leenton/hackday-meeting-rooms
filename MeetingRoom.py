af3f60d4ea584b3fff3b6992a63eecf766de2986
Leenton Chinyanga <Leenton.Chinyanga@futurenet.com>
2024-04-04 14:46:04 +0200

    Initialize database

---
+++ MeetingRoom.py
@@ -0,0 +1,5 @@
+Class MeetingRoom():
+    def __init__(self, id, name, floor):
+        self.id = room_id
+        self.name = room_name
+        self.floor = room_capacity
--- Routes.py
+++ Routes.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, url_for, redirect, request, session, jsonify, json
-import os
-import random
+from os import path
 import sqlite3
 from Office import Office
 
@@ -8,21 +7,29 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = "684a2c31fa1f159e791fbd0d01e4214c58b1ba170543bd7085dd61c722617f9f"
 
+def intialize_database() -> None:
+    if not path.exists("data.db"):
+        conn = sqlite3.connect("data.db")
+        c = conn.cursor()
 
-#Check if data.db exists, if not create it
-if not os.path.exists("data.db"):
-    conn = sqlite3.connect("data.db")
-    c = conn.cursor()
+        #get the contents of the schema file
+        with open("schema.sql") as f:
+            schema = f.read()
+        
+        c.executescript(schema)
+        c.close()
+        conn.commit()
 
-    #get the contents of the schema file
-    with open("schema.sql") as f:
-        schema = f.read()
-    
-    c.executescript(schema)
-    conn.commit()
-    conn.close()
+        #add the sample data in data.sql to the database
+        c = conn.cursor()
 
+        with open("data.sql") as f:
+            data = f.read()
 
+        c.executescript(data)
+        conn.commit()
+        c.close()
+        conn.close()
 
 def get_offices() -> list[Office]:
     conn = sqlite3.connect("data.db")
@@ -41,5 +48,6 @@ def home():
 def anison():
     return render_template("building.html")
 
+intialize_database()
 app.run(debug=True)
 
