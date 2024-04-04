97958a3b24e7a3f7131f693e2fd000bdecaf83ec
UserHasBeenName <sonny.pugh.125@accesstomusic.ac.uk>
2024-04-04 15:37:32 +0100

    add self parameter to get_meeting_room, so function may reference self.id, and import Issue

--- Floor/Floor.py
+++ Floor/Floor.py
@@ -1,12 +1,13 @@
 import sqlite3
 from MeetingRoom import MeetingRoom
+from Issue import Issue
 
 class Floor():
     def __init__(self, id: int, floor_num: int):
         self.id = id
         self.floor_num = floor_num
     
-    def get_meeting_room() -> list[MeetingRoom]:
+    def get_meeting_room(self) -> list[MeetingRoom]:
         conn = sqlite3.connect("data.db")
         c = conn.cursor()
         c.execute("SELECT issue_name, issue_id FROM issues WHERE room_id = ?", (self.id,))
